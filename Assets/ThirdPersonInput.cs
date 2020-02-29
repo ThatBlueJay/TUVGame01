a32322579f543a0aadcdea5a120a481b63048ad3
BuildTools <unconfigured@null.spigotmc.org>
2020-02-29 15:07:37 -0500

    First Commit
    Signed-off-by: BuildTools <unconfigured@null.spigotmc.org>

---
+++ Assets/ThirdPersonInput.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class ThirdPersonInput : MonoBehaviour
+{
+
+    public FixedJoystick LeftJoystick;
+    public FixedTouchField TouchField;
+    public FixedButton Button;
+    protected ThirdPersonUserControl Control;
+
+    protected float CameraAngle;
+    protected float CameraAngleSpeed = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Control = GetComponent<ThirdPersonUserControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Control.m_Jump = Button.Pressed;
+        Control.Hinput = LeftJoystick.inputVector.x;
+        Control.Vinput = LeftJoystick.inputVector.y;
+
+        CameraAngle += TouchField.TouchDist.x * CameraAngleSpeed;
+
+        Camera.main.transform.position = transform.position + Quaternion.AngleAxis(CameraAngle,Vector3.up) * new Vector3(0, 3, 4);
+        Camera.main.transform.rotation = Quaternion.LookRotation(transform.position + Vector3.up * 2f - Camera.main.transform.position, Vector3.up);
+
+
+    }
+}
