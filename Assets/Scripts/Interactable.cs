7da27048ec763d70c5dc712be4faecb0a0c80438
BuildTools <unconfigured@null.spigotmc.org>
2020-03-09 22:12:07 -0400

    Update #1 (Major changes)

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,49 @@
+﻿
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public float radius = 3f;
+    public Transform interactionTransform;
+
+    bool isFocus = false;
+    Transform player;
+
+    bool hasInteracted = false;
+
+    public virtual void Interact ()
+    {
+        // This method is for overwritting for different interactables
+    }
+    private void Update()
+    {
+        if (isFocus && !hasInteracted)
+        {
+            float distance = Vector3.Distance(player.position, interactionTransform.position);
+            if (distance <= radius)
+            {
+                Interact();
+                hasInteracted = true;
+            }
+        }
+    }
+    public void OnFocused (Transform playerTranform)
+    {
+        isFocus = true;
+        player = playerTranform;
+        hasInteracted = false;
+    }
+
+    public void OnDefocused ()
+    {
+        isFocus = false;
+        player = null;
+        hasInteracted = false;
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(interactionTransform.position, radius);
+
+    }
+}
